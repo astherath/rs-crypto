1baa268913dfe2943b1522e2f89471d1409d10c2
astherath <farceriv@gmail.com>
2020-12-03 14:34:40 -0500

    totally refactored the list data structure (now working).
    This was a MASSIVE lesson in a couple of things:
    1. rust doesn't play nice with pointers and raw references (at all).
    2. Linked lists are overhyped and [the benefits they provide are overrated](https://rust-unofficial.github.io/too-many-lists/)
    3. Vec is actually an excellent collection.
    
    The refactor was incredibly easy mostly due to how abstracted all of the other code was from the implementation of the actual list. This is a very good sign and shows that the design was proper.

--- lib/blockchain/mod.rs
+++ lib/blockchain/mod.rs
@@ -1,7 +1,7 @@
 //! This data structure _could_ be made to include a generic but it doesn't really make
 //! sense to do all of that work without purpose - we're not ever gonna use this with a
 //! non-block piece of data, so it's not worth the extra effort.
-use append_only_linked_list::List;
+use append_only_list::List;
 use std::fmt;
 mod block;
 pub use block::{Block, BlockChainData};
@@ -16,13 +16,14 @@ impl Blockchain {
         Self { block_list }
     }
 
-    pub fn append_block(self, block: Block) {
+    pub fn append_block(&mut self, block: Block) {
         self.block_list.append_block(block);
     }
 }
 
 impl fmt::Display for Blockchain {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        // FIXME
         let display_data = String::new();
         write!(f, "{}", display_data)
     }
@@ -32,57 +33,29 @@ impl fmt::Display for Blockchain {
 /// the commit history, but it introduced an unnecessary amount of 3-way coupling between
 /// this, the list, and the block modules. This would make it very easy to extract to another
 /// module in the future if it grows to be unmanageable but this works better for now.
-mod append_only_linked_list {
+mod append_only_list {
     use super::block::Block;
 
     pub struct List {
-        data: Option<Block>,
-        next: Option<Box<Self>>,
+        data_list: Vec<Block>,
     }
 
     impl List {
-        /// Instanciates an **empty** linked list.
+        /// Instanciates an **empty** list.
         /// Blocks are the only data this structure interacts with.
         pub fn new() -> Self {
             Self {
-                data: None,
-                next: None,
+                data_list: Vec::new(),
             }
         }
 
-        /// Private function to create an instance based on a block
-        fn from_block(block: &Block) -> Self {
-            Self {
-                data: Some(block.clone()),
-                next: None,
-            }
+        pub fn append_block(&mut self, block: Block) {
+            self.data_list.push(block);
         }
 
-        pub fn append_block(mut self, block: Block) {
-            if self.is_empty() {
-                self.data = Some(block);
-            } else {
-                let mut last_node = self.get_last_block();
-                let new_node = Self::from_block(&block);
-                last_node.next = Some(Box::new(new_node));
-            }
+        fn get_most_recent_block(&self) ->  Option<&Block> {
+            self.data_list.last()
         }
 
-        fn get_last_block(self) -> Self {
-            let mut current = self.next;
-            loop {
-                match current {
-                    None => break,
-                    Some(_) => current = current.unwrap().next,
-                }
-            }
-            *current.unwrap()
-        }
-
-        /// This isn't a _super_ necessary method but it makes it a little easier to
-        /// refactor in the future if we change to node based list (or something else).
-        fn is_empty(&self) -> bool {
-            self.data.is_none()
-        }
     }
 }
